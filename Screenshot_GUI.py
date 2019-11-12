ade79287305e1c29a8ef89fea9c3d8351bc193ad
ccs96307 <ccs96307@gmail.com>
2019-11-12 21:30:10 +0800

    Screenshot will copy to clipboard. (Windows)

--- Screenshot_GUI.py
+++ Screenshot_GUI.py
@@ -3,12 +3,18 @@ import sys
 import time
 import numpy as np
 import cv2
+from io import BytesIO
+
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from Screenshot import Ui_MainWindow
-from PIL import ImageGrab
+from PIL import Image, ImageGrab
+
+import win32clipboard as clip
+import win32con
+
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -26,20 +32,31 @@ class MainWindow(QtWidgets.QMainWindow):
     def addANewScreenshot(self, mode=1):
         if mode == 1:
             self.setVisible(False)
-            time.sleep(0.2)
+            time.sleep(0.3)
 
             image = ImageGrab.grab()
-            image = np.array(image)
+            image_np = np.array(image)
 
-            height, width, bytesPerComponents = image.shape
+            height, width, bytesPerComponents = image_np.shape
             bytesPerLine = 3*width
 
-            QImg = QImage(image.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            QImg = QImage(image_np.data, width, height, bytesPerLine, QImage.Format_RGB888)
             pixmap = QPixmap.fromImage(QImg)
             self.ui.label.resize(QSize(width, height))
             self.ui.label.setPixmap(pixmap)
             self.ui.label.setScaledContents(True)
             self.showMaximized()
+
+            # ClipBoard
+            output = BytesIO()
+            image.convert('RGB').save(output, 'BMP')
+            data = output.getvalue()[14:]
+            output.close()
+
+            clip.OpenClipboard()
+            clip.EmptyClipboard()
+            clip.SetClipboardData(win32con.CF_DIB, data)
+            clip.CloseClipboard()
 
     def exitEvent(self):
         exit()
