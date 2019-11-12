0df45c6034a5660414fc383017d9f252cb28ff84
ccs96307 <ccs96307@gmail.com>
2019-11-12 15:42:14 +0800

    Basic Screenshot is finished.

--- Screenshot_GUI.py
+++ Screenshot_GUI.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+import sys
+import time
+import numpy as np
+import cv2
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from Screenshot import Ui_MainWindow
-import sys
+from PIL import ImageGrab
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -16,14 +20,29 @@ class MainWindow(QtWidgets.QMainWindow):
         self.add = QShortcut(QKeySequence("Ctrl+N"), self)
         self.add.activated.connect(self.addANewScreenshot)
 
-    def addANewScreenshot(self):
+        self.exit = QShortcut(QKeySequence("Ctrl+D"), self)
+        self.exit.activated.connect(self.exitEvent)
 
+    def addANewScreenshot(self, mode=1):
+        if mode == 1:
+            self.setVisible(False)
+            time.sleep(0.2)
 
+            image = ImageGrab.grab()
+            image = np.array(image)
 
-class sub_MainWindow(QMainWindow):
-    def __init__(self, pic):
-        super(sub_MainWindow, self).__init__()
+            height, width, bytesPerComponents = image.shape
+            bytesPerLine = 3*width
 
+            QImg = QImage(image.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(QImg)
+            self.ui.label.resize(QSize(width, height))
+            self.ui.label.setPixmap(pixmap)
+            self.ui.label.setScaledContents(True)
+            self.showMaximized()
+
+    def exitEvent(self):
+        exit()
 
 
 if __name__ == '__main__':
